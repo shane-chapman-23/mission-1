e2a700d335af00de92a534bc6382edf5c87f847b
shane-chapman-23 <chapmanshane23@gmail.com>
2025-08-15 09:09:21 +1200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>
+        TURNERS CAR <br />
+        INSURANCE
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Chatbot.jsx
@@ -0,0 +1,5 @@
+import styles from "./Chatbot.module.css";
+
+export default function Chatbot() {
+  return <div></div>;
+}
